44b2d1aa7d2fd1f93466e62e5a0241b87e23a5d0
dyogojaa <dyogojaa@gmail.com>
2024-02-09 17:17:58 -0300

    Criação de Login

--- app.py
+++ app.py
@@ -2,11 +2,16 @@ from flask import Flask
 from flask_restful import Api
 from resources.hotel import Hoteis, Hotel
 from sql_alchemy import banco
+from resources.usuario import User, UserRegister, UserLogin
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'DontTellAnyone'
 api = Api(app)
+jwt = JWTManager(app)
+
 
 # Inicializa o banco de dados no contexto da aplicação Flask
 with app.app_context():
@@ -15,6 +20,10 @@ with app.app_context():
 
 api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
+api.add_resource(User, '/usuarios/<int:user_id>')
+api.add_resource(UserRegister, '/cadastro')
+api.add_resource(UserLogin, '/login')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
